168d8835e21b1bdd7b18e0aa3bbe3d532707b240
Nirit Nagar <nirit.nn@gmail.com>
2023-08-13 16:34:25 +0200

    update responsive view

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
-import { render } from 'react-dom';
-import axios from 'axios';
-import nightIcon from './img/moon.svg';
-import dayIcon from './img/sun.svg';
-import './style.css';
+import React, { useState } from "react";
+import { render } from "react-dom";
+import axios from "axios";
+import nightIcon from "./img/moon.svg";
+import dayIcon from "./img/sun.svg";
+import "./style.css";
 
 const apikey = process.env.REACT_APP_API_KEY;
 
@@ -16,15 +16,14 @@ const WeatherApp = () => {
   const [tabState, setTabState] = useState(0);
   const toggleTab = (index) => {
     setTabState(index);
-}
+  };
 
- const arrTabs = ["Berlin", "Paris", "New York", "London"];
-
+  const arrTabs = ["Berlin", "Paris", "New York", "London"];
 
   const getWeatherData = (city) => {
     axios({
       method: "GET",
-      url:  `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apikey}`,
+      url: `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&APPID=${apikey}`,
     })
       .then((response) => {
         // setCity(response.data.name);
@@ -35,66 +34,81 @@ const WeatherApp = () => {
       .catch((error) => {
         console.log(error);
       });
-
   };
-
-
 
   return (
     <>
-    <div className={`bg-img ${city}`}></div>
+      <div className={`bg-img ${city}`}></div>
       <div className="wrap-box">
-        <div className="weather-box">
         <div className="tabs-wrap">
-            <div className="tabs">
-            {arrTabs.map((e,i) => {
-              return  <button key={i}
-              className={tabState === i ? "tabs button active-tab" : "tabs button"}
-              onClick={() => {
-                setCity(e); toggleTab(i)
-              }}>
-              {e}</button>
+          <div className="tabs">
+            {arrTabs.map((e, i) => {
+              return (
+                <button
+                  key={i}
+                  className={
+                    tabState === i ? "tabs button active-tab" : "tabs button"
+                  }
+                  onClick={() => {
+                    setCity(e);
+                    toggleTab(i);
+                  }}
+                >
+                  {e}
+                </button>
+              );
             })}
+          </div>
+        </div>
 
-            </div>
+        <div className="weather-box">
+          <p style={{ marginBottom: "30px" }}>
+            <span className="header">
+              <strong>{city}</strong>
+            </span>
+            <span className="date"> {new Date().toLocaleString()}</span>
+          </p>
+
+          <div>
+            {icon === "01n" ? (
+              <img
+                src={nightIcon}
+                className={icon === "01n" ? "night-time" : "weather-icon"}
+                alt="weather-icon"
+                height="55"
+                width="55"
+              />
+            ) : icon === "01d" ? (
+              <img
+                src={dayIcon}
+                className={icon === "01d" ? "day-time" : "weather-icon"}
+                alt="weather-icon"
+                height="65"
+                width="65"
+              />
+            ) : (
+              <img
+                src={`https://openweathermap.org/img/wn/${icon}@2x.png`}
+                className="weather-icon"
+                alt="weather-icon"
+              />
+            )}
           </div>
 
-            <p style={{ marginBottom: "30px" }}>
-              <span className="header"><strong>{city}</strong></span>
-              <span className="date">  {new Date().toLocaleString()}</span>
-            </p>
+          <p className="temp">
+            {temperature}
+            <span>
+              <strong>o </strong>
+            </span>
+          </p>
 
-        <div>
-          { icon === "01n"
-            ?
-            <img src={nightIcon} className={icon === "01n" ? "night-time" : "weather-icon" } alt="weather-icon" height="55" width="55" />
-            :
-            icon === "01d"
-            ?
-            <img src={dayIcon} className={icon === "01d" ? "day-time" : "weather-icon" } alt="weather-icon" height="65" width="65" />
-            :
-            <img src={ `https://openweathermap.org/img/wn/${icon}@2x.png` } className="weather-icon" alt="weather-icon" />
-          }
+          <p className="desc">{desc}</p>
         </div>
+      </div>
 
-            <p className="temp">
-        {temperature}
-        <span><strong>o </strong></span>
-                </p>
-
-
-                 <p className="desc">
-                    {desc}
-                  </p>
-        </div>
-
-</div>
-{ getWeatherData(city) }
-
+      {getWeatherData(city)}
     </>
   );
-
 };
-
 
 render(<WeatherApp />, document.querySelector("#root"));
